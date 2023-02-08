e254620763055df09708514801af7eacb8cd73d4
csehg0312 <62523554+csehg0312@users.noreply.github.com>
2023-02-08 15:51:50 +0100

    Modified disk.py

--- disk.py
+++ disk.py
@@ -2,7 +2,7 @@ import shutil
 
 total, used, free = shutil.disk_usage("/")
 
-total = (total // (2**30))
+#total = (total // (2**30))
 
 if __name__ == '__main__':
     print('Total: %d GiB' % (total // (2**30)))
